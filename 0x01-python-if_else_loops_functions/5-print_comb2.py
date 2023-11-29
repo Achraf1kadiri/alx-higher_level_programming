868cb3973b881eed2d4c93d8eed7d1f52f88295b
Achraf1kadiri <achraf1kadiri1@gmail.com>
2023-11-29 11:11:15 -0800

    fixx

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for number in range(0, 100):
     if number == 99:
         print("{0}".format(number), end="\n")
     else:
-        print("{0}, ".format(number), end="")
+        print("{0}".format(number), end="")
